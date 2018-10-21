e31224615d490e5827cd06f76f581ca57b6945b5
Sando Geek <sandogeek@163.com>
2018-10-22 01:07:55 +0800

    简单重构代码并增加关于订阅时的参数应采用异步方式的注释

--- src/app/shared/service/web-socket-service.service.ts
+++ src/app/shared/service/web-socket-service.service.ts
@@ -20,76 +20,33 @@ export class WebSocketService {
   constructor() {
   }
 
-  // 返回一个可观测的流，包括服务器返回的消息
+  // 连接服务器，初始化一个可观察者（消息源）
   createObservableSocket = (url: string) => {
     if (this.conneted === false) {
       // 连接服务器
       this.ws = new WebSocket(url);
+      // 设定发送的数据为二进制数据
       this.ws.binaryType = 'arraybuffer';
       this.observable = new Observable<any>(this.onSubscribe);
     }
   }
   // 当this.observable被.subscribe(x)调用,subscriber = x
   onSubscribe = (subscriber: Subscriber<any>) => {
-    // 添加观察者
+    // 添加观察者，因为使用的是循环subscribers,调用每个next，所以他们是同步的，如果前一个next发生耗时操作，会导致
+    // 后一个订阅者无法及时接收到消息，可能导致问题，最好让每个观察者中的next error complete变成异步执行
     this.subscribers.push(subscriber);
     // console.log(this.subscribers.length);
-    // Websocket回调函数只绑定一次就够了
+    // Websocket生命周期回调函数只绑定一次就够了
     if (this.subscribers.length === 1) {
-      this.ws.onopen = (event) => {
-        // console.log(`进来了`);
-        this.conneted = true;
-        // 此处会触发x.next回调函数
-        this.subscribers.forEach((value, index, subscribers) => {
-            value.next('连接到服务器成功');
-        });
-      };
-      // 接收响应消息时回调
-      this.ws.onmessage = (event) => {
-        // 查看收发数据是否一致
-        // console.log(`实际接收的数据：${Array.prototype.map.call(new Uint8Array(event.data), x => x.toString(10)).join(',')}`);
-        // 把event.data转化为相应的类对象
-        if (event.data as ArrayBuffer) {
-          /**
-           * 0-3 包长 4-5 packetId 6-最后 probuf编码的对象
-           */
-          const packetLength = (event.data as ArrayBuffer).byteLength;
-          const dataView = new DataView(event.data);
-          const packetId = dataView.getUint16(4);
-          const data = new Uint8Array(event.data, 6);
-          // 验证包长是否正确
-          if ( packetLength !== dataView.getUint32(0) ) {
-            console.error(`packetId[${packetId}]包长不对`);
-            return;
-          }
-          const messageClass = PacketId.packetId2Class.get(packetId);
-          if (messageClass === undefined) {
-            console.error(`PacketId中不存在id为${packetId}的包`);
-          }
-          const obj = messageClass.decode(data);
-          // console.log(`接收到的包：packetId[${packetId}] 类名[${messageClass.name}] 内容\n${JSON.stringify(obj)}`);
-          this.subscribers.forEach((value, index, subscribers) => {
-            const nextData = {
-              packetId: packetId,
-              clazz : messageClass,
-              resp: obj
-            };
-            value.next(nextData);
-            // value.next('连接到服务器成功');
-          });
-        }
-      };
-      this.ws.onerror = (event) => subscriber.error(event);
-      this.ws.onclose = (event) => {
-        this.conneted = false;
-        subscriber.complete();
-      };
+      this.bindWsLifecircle(subscriber);
     }
     const myUnsubscribable = new MyUnsubscribable();
     myUnsubscribable.outer = this;
     // 同时需要外部的this,以及调用这个函数的this怎么解决
     // 把外部的this当作对象的属性传进去
     myUnsubscribable.unsubscribe = function(this: MyUnsubscribable) {
+      // console.log(this);
+      // 移除对应的MyUnsubscribable
       const index = this.outer.myUnsubscribables.indexOf(this);
       // console.log(index);
       // console.log(this.outer.myUnsubscribables);
@@ -97,12 +54,69 @@ export class WebSocketService {
         this.outer.myUnsubscribables.splice(index, 1);
       }
       // console.log(this.outer.myUnsubscribables);
-      // console.log(this);
     };
     this.myUnsubscribables.push(myUnsubscribable);
     return myUnsubscribable;
   }
-  // 断开与服务器的连接
+  /**
+   * 绑定websocket生命周期回调函数
+   */
+  private bindWsLifecircle = (subscriber: Subscriber<any>) => {
+    this.ws.onopen = (event) => {
+      // console.log(`进来了`);
+      this.conneted = true;
+      // 此处会触发x.next回调函数
+      this.subscribers.forEach((value, index, subscribers) => {
+          value.next('连接到服务器成功');
+      });
+    };
+    // 接收响应消息时回调
+    this.ws.onmessage = (event) => {
+      // 查看收发数据是否一致
+      // console.log(`实际接收的数据：${Array.prototype.map.call(new Uint8Array(event.data), x => x.toString(10)).join(',')}`);
+      // 把event.data转化为相应的类对象
+      if (event.data as ArrayBuffer) {
+        this.decodeWebSocketBinaryFrame(event);
+      }
+    };
+    this.ws.onerror = (event) => subscriber.error(event);
+    this.ws.onclose = (event) => {
+      this.conneted = false;
+      subscriber.complete();
+    };
+  }
+  /**
+   * 解码服务器发过来的WebSocketBinaryFrame
+   */
+  private decodeWebSocketBinaryFrame = (event: MessageEvent) => {
+    /**
+     * 0-3 包长 4-5 packetId 6-最后 probuf编码的对象
+     */
+    const packetLength = (event.data as ArrayBuffer).byteLength;
+    const dataView = new DataView(event.data);
+    const packetId = dataView.getUint16(4);
+    const data = new Uint8Array(event.data, 6);
+    // 验证包长是否正确
+    if ( packetLength !== dataView.getUint32(0) ) {
+      console.error(`packetId[${packetId}]包长不对`);
+      return;
+    }
+    const messageClass = PacketId.packetId2Class.get(packetId);
+    if (messageClass === undefined) {
+      console.error(`PacketId中不存在id为${packetId}的包`);
+    }
+    const obj = messageClass.decode(data);
+    // console.log(`接收到的包：packetId[${packetId}] 类名[${messageClass.name}] 内容\n${JSON.stringify(obj)}`);
+    this.subscribers.forEach((value, index, subscribers) => {
+      const nextData = {
+        packetId: packetId,
+        clazz : messageClass,
+        resp: obj
+      };
+      value.next(nextData);
+    });
+  }
+  // 正常地断开与服务器的连接
   disconnect = () => {
     if (this.conneted === true) {
       this.ws.close(1000, '溜了溜了，下线');
