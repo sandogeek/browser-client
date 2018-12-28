10185a4c3efd5bc42b6605b4c451385120ab081b
sandogeek <sandogeek@163.com>
2018-12-28 18:33:18 +0800

    场景切换

--- src/app/gaming/gaming.component.ts
+++ src/app/gaming/gaming.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { WebSocketService, CustomMessage } from '../shared/service/web-socket-service.service';
 import { PartialObserver } from 'rxjs';
-import {  EnterWorldReq, RoleUiInfoResp, ObjectDisappearResp } from '../shared/model/proto/bundle';
+import {  EnterWorldReq, RoleUiInfoResp,
+  ObjectDisappearResp, SceneUiInfoResp, ISceneCanGoInfo, SwitchSceneReq } from '../shared/model/proto/bundle';
 import { Role } from '../choose-role/model/Role';
+import { RoleService } from '../shared/service/role.service';
 
 @Component({
   selector: 'app-gaming',
@@ -11,9 +13,18 @@ import { Role } from '../choose-role/model/Role';
 })
 export class GamingComponent implements OnInit {
   roles: Array<Role> = new Array();
+  sceneCanGoInfos: Array<ISceneCanGoInfo> = new Array();
   currentSceneName: string;
+  sceneSwitchFlag = false;
+  selfInfo = [
+    `昵称：${this.myName}`,
+    `角色类型：${this.myRoleTypeName}`,
+    `等级：${this.myLevel}`,
+    `血量：${this.myCurrentHp}/${this.myMaxHp}`,
+    `蓝量：${this.myCurrentMp}/${this.myMaxMp}`
+  ];
 
-  sceneUiInfoObserver: PartialObserver<CustomMessage> = {
+  infoObserver: PartialObserver<CustomMessage> = {
     next : message => {
       // console.log(`${JSON.stringify(message.resp)}`);
       if (message.clazz === RoleUiInfoResp) {
@@ -24,6 +35,10 @@ export class GamingComponent implements OnInit {
         role.level = roleUiInfoResp.level;
         role.roleType = roleUiInfoResp.roleType;
         this.roles = [ ...this.roles, role];
+      } else if (message.clazz === SceneUiInfoResp) {
+        const sceneUiInfo = <SceneUiInfoResp>message.resp;
+        this.currentSceneName = sceneUiInfo.sceneName;
+        this.sceneCanGoInfos = sceneUiInfo.sceneCanGoInfos;
       }
     },
     error: err => console.log(err),
@@ -40,13 +55,39 @@ export class GamingComponent implements OnInit {
   };
   constructor(
     private wsService: WebSocketService,
+    private roleService: RoleService,
   ) {
-    this.wsService.observable.subscribe(this.sceneUiInfoObserver);
+    this.wsService.observable.subscribe(this.infoObserver);
     this.wsService.observable.subscribe(this.objectDisappearObserver);
     wsService.sendPacket(EnterWorldReq, {});
   }
 
-  ngOnInit() {
+  switchTo = (sceneId: number) => {
+    this.roles = new Array();
+    this.wsService.sendPacket(SwitchSceneReq, {targetSceneId: sceneId});
   }
 
+  ngOnInit() {
+  }
+  get myName() {
+    return this.roleService.selectedRole.name;
+  }
+  get myLevel() {
+    return this.roleService.selectedRole.level;
+  }
+  get myRoleTypeName() {
+    return this.roleService.selectedRole.roleTypeName;
+  }
+  get myCurrentHp() {
+    return this.roleService.selectedRole.currentHp;
+  }
+  get myMaxHp() {
+    return this.roleService.selectedRole.maxHp;
+  }
+  get myCurrentMp() {
+    return this.roleService.selectedRole.currentMp;
+  }
+  get myMaxMp() {
+    return this.roleService.selectedRole.maxMp;
+  }
 }
--- src/app/shared/model/packet/PacketId.ts
+++ src/app/shared/model/packet/PacketId.ts
@@ -1,5 +1,5 @@
 
-import { ChatReq, ChatResp, LoginAuthReq, LoginResultResp, CustomRoleUiInfoResp, RoleUiInfoResp, RegisterReq, RegisterResp, AddRoleReq, AddRoleResp, ChooseRoleReq, ChooseRoleResp, DeleteRoleReq, DeleteRoleResp, GetRoleListReq, GetRoleListResp, EnterWorldReq, ObjectDisappearResp, PingHeartBeat, PongHeartBeat } from '../proto/bundle';
+import { ChatReq, ChatResp, LoginAuthReq, LoginResultResp, CustomRoleUiInfoResp, MonsterUiInfoResp, RoleUiInfoResp, RegisterReq, RegisterResp, AddRoleReq, AddRoleResp, ChooseRoleReq, ChooseRoleResp, DeleteRoleReq, DeleteRoleResp, GetRoleListReq, GetRoleListResp, EnterWorldReq, ObjectDisappearResp, SceneUiInfoResp, SwitchSceneReq, PingHeartBeat, PongHeartBeat } from '../proto/bundle';
 import { Type } from '@angular/core';
 import { Reader, Writer } from 'protobufjs';
 import { IPacket } from '../proto/IPacket';
@@ -25,6 +25,8 @@ export class PacketId {
     static readonly LOGIN_RESULT_RESP: number = 10002;
     /** 自身信息*/
     static readonly CUSTOM_ROLE_UI_INFO_RESP: number = 10502;
+    /** 怪物提供给前端的ui信息响应包*/
+    static readonly MONSTER_UI_INFO_RESP: number = 10503;
     /** 角色UI信息响应包*/
     static readonly ROLE_UI_INFO_RESP: number = 10501;
     /** 注册请求*/
@@ -51,6 +53,10 @@ export class PacketId {
     static readonly ENTER_WORLD_REQ: number = 10403;
     /** 可见物消失响应包*/
     static readonly OBJECT_DISAPPEAR_RESP: number = 10402;
+    /** 场景信息响应包*/
+    static readonly SCENE_UI_INFO_RESP: number = 10404;
+    /** 切换场景请求*/
+    static readonly SWITCH_SCENE_REQ: number = 10401;
     /** ping心跳包*/
     static readonly PING_HEART_BEAT: number = 1001;
     /** pong心跳包*/
@@ -64,6 +70,7 @@ export class PacketId {
         PacketId.put(PacketId.LOGIN_AUTH_REQ, LoginAuthReq);
         PacketId.put(PacketId.LOGIN_RESULT_RESP, LoginResultResp);
         PacketId.put(PacketId.CUSTOM_ROLE_UI_INFO_RESP, CustomRoleUiInfoResp);
+        PacketId.put(PacketId.MONSTER_UI_INFO_RESP, MonsterUiInfoResp);
         PacketId.put(PacketId.ROLE_UI_INFO_RESP, RoleUiInfoResp);
         PacketId.put(PacketId.REGISTER_REQ, RegisterReq);
         PacketId.put(PacketId.REGISTER_RESP, RegisterResp);
@@ -77,6 +84,8 @@ export class PacketId {
         PacketId.put(PacketId.GET_ROLE_LIST_RESP, GetRoleListResp);
         PacketId.put(PacketId.ENTER_WORLD_REQ, EnterWorldReq);
         PacketId.put(PacketId.OBJECT_DISAPPEAR_RESP, ObjectDisappearResp);
+        PacketId.put(PacketId.SCENE_UI_INFO_RESP, SceneUiInfoResp);
+        PacketId.put(PacketId.SWITCH_SCENE_REQ, SwitchSceneReq);
         PacketId.put(PacketId.PING_HEART_BEAT, PingHeartBeat);
         PacketId.put(PacketId.PONG_HEART_BEAT, PongHeartBeat);
         // end
